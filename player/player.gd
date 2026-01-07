3d2773f7ea3ac2600448a300c7c180ee80ececc9
The YaviK Team <theyavikteam@gmail.com>
2026-01-07 17:11:09 +0100

    Hurt Player Management

--- player/player.gd
+++ player/player.gd
@@ -3,12 +3,20 @@ extends CharacterBody2D
 signal healthChanged
 
 @export var speed: int = 35
-@export var maxHealth = 3
+@export var maxHealth: int = 3
+@export var knockbackPower: int = 500
+
+var isHurt: bool = false
+var enemyCollisions = []
 
 @onready var animations = $AnimationPlayer
-@onready var hurtColor = $Sprite2D/ColorRect
+@onready var effects = $Effects
+@onready var hurtTimer = $HurtTimer
 
 @onready var currentHealth: int = maxHealth
+
+func _ready():
+	effects.play("RESET")
 
 func handleInput():
 	var moveDirection = Input.get_vector( "ui_left", "ui_right", "ui_up", "ui_down")
@@ -29,8 +37,33 @@ func _physics_process(delta):
 	handleInput()
 	move_and_slide()
 	updateAnimation()
+	if !isHurt:
+		for enemyArea in enemyCollisions:
+			hurtByEnemy(enemyArea)
 		
 func _on_hurt_box_area_entered(area):
 	if area.name == "HitBox":
-		currentHealth -= 1
-		healthChanged.emit(currentHealth)
+		enemyCollisions.append(area)
+		
+func knockback(enemyVelocity: Vector2):
+	var knockbackDirection = (enemyVelocity-velocity).normalized() * knockbackPower
+	velocity = knockbackDirection
+	move_and_slide()
+	
+func hurtByEnemy(area):
+	currentHealth -= 1
+	if currentHealth < 0:
+		currentHealth = maxHealth
+		
+	healthChanged.emit(currentHealth)
+	isHurt = true
+	knockback(area.get_parent().velocity)
+	effects.play("hurtBlink")
+	hurtTimer.start()
+	await hurtTimer.timeout
+	effects.play("RESET")
+	isHurt = false
+
+
+func _on_hurt_box_area_exited(area):
+	enemyCollisions.erase(area) # Replace with function body.
